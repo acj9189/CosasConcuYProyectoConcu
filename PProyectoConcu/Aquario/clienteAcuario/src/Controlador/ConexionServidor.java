5ae5ee3dc90f06cf213cc8df85437d3916137420
acj9189 <anecardenasja@unal.edu.co>
2018-01-08 16:46:41 -0500

    Actu

--- PProyectoConcu/Aquario/clienteAcuario/src/Controlador/ConexionServidor.java
+++ PProyectoConcu/Aquario/clienteAcuario/src/Controlador/ConexionServidor.java
@@ -5,7 +5,13 @@
  */
 package Controlador;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -14,6 +20,52 @@ import java.net.Socket;
 public class ConexionServidor {
     
     private Socket Socket;
+    private int Port;
+    private String Address;
+    
+    public PrintWriter theOut;
+    public BufferedReader theIn;
+
+    public ConexionServidor() {
+    }
+
+    public ConexionServidor( int Port, String Address) {
+        
+        this.Port = Port;
+        this.Address = Address;
+        try {
+            this.Socket = new Socket(this.Address, this.Port);
+            this.theOut = new PrintWriter(this.Socket.getOutputStream(), true);
+            this.theIn = new BufferedReader(new InputStreamReader(this.Socket.getInputStream(), "UTF-8"));
+        } catch (IOException ex) {
+            Logger.getLogger(ConexionServidor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public String Write(String Comando){
+        String Retorno ="";
+        try {
+            this.theOut.println(Comando);
+            Retorno = this.theIn.readLine();
+        } catch (IOException ex) {
+            Logger.getLogger(ConexionServidor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return Retorno;
+    }
+    
+    public String Read(){
+        String Retorno ="";
+        try {
+            Retorno = this.theIn.readLine();
+        } catch (IOException ex) {
+            Logger.getLogger(ConexionServidor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return Retorno;
+    }
+    
+    
+    
+    
     
     
 }
