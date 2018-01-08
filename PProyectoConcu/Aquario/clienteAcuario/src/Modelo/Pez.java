fa02473d8d0e94b7df9f3e3cbd7efff43f903d71
acj9189 <anecardenasja@unal.edu.co>
2018-01-08 15:59:19 -0500

    actu

--- PProyectoConcu/Aquario/clienteAcuario/src/Modelo/Pez.java
+++ PProyectoConcu/Aquario/clienteAcuario/src/Modelo/Pez.java
@@ -16,4 +16,39 @@ public class Pez {
     private int y;
     private Image imagen;
 
+    public Pez() {
+    }
+
+    public Pez(int x, int y, Image imagen) {
+        this.x = x;
+        this.y = y;
+        this.imagen = imagen;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public Image getImagen() {
+        return imagen;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setImagen(Image imagen) {
+        this.imagen = imagen;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    
+
 }
