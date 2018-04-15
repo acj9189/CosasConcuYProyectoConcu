78875ef4efd1a7a7466905dfb70f2553d158ccf5
Anderson Peña <anderson.1701323495@ucaldas.edu.co>
2018-04-15 14:40:52 -0500

    CU Estudiantes

---
+++ GISDES/GISDES/Controllers/EstudiantesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GISDES.Models;
+
+namespace GISDES.Controllers
+{
+    public class EstudiantesController : Controller
+    {
+        private GISDESEntity db = new GISDESEntity();
+
+        // GET: Estudiantes
+        public ActionResult Index()
+        {
+            return View(db.Estudiantes.ToList());
+        }
+
+        // GET: Estudiantes/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Estudiantes estudiantes = db.Estudiantes.Find(id);
+            if (estudiantes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(estudiantes);
+        }
+
+        // GET: Estudiantes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Estudiantes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,Nombres,Apellidos,FechaNacimiento,Estudios,Celular,Correo")] Estudiantes estudiantes)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Estudiantes.Add(estudiantes);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(estudiantes);
+        }
+
+        // GET: Estudiantes/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Estudiantes estudiantes = db.Estudiantes.Find(id);
+            if (estudiantes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(estudiantes);
+        }
+
+        // POST: Estudiantes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Nombres,Apellidos,FechaNacimiento,Estudios,Celular,Correo")] Estudiantes estudiantes)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(estudiantes).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(estudiantes);
+        }
+
+        // GET: Estudiantes/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Estudiantes estudiantes = db.Estudiantes.Find(id);
+            if (estudiantes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(estudiantes);
+        }
+
+        // POST: Estudiantes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Estudiantes estudiantes = db.Estudiantes.Find(id);
+            db.Estudiantes.Remove(estudiantes);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
